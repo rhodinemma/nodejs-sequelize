a6047b759f848c0745de0952de11b901137f5a14
rhodinemma <63339234+rhodinemma@users.noreply.github.com>
2022-08-27 17:13:10 +0300

    create one-to-many relationship

---
+++ one_to_many.js
@@ -0,0 +1,83 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize("student_db", "root", "", {
+  host: "localhost",
+  dialect: "mariadb",
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection established successfully");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to database ", error);
+  });
+
+const Student = sequelize.define("students", {
+  student_id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+const Grade = sequelize.define("grades", {
+  grade: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+const grade_data = [{ grade: 9 }, { grade: 10 }, { grade: 11 }];
+
+const student_data = [
+  { name: "John Baker", gradeId: 2 },
+  { name: "Max Butler", gradeId: 1 },
+  { name: "Ryan Fisher", gradeId: 3 },
+  { name: "Robert Gray", gradeId: 2 },
+  { name: "Sam Lewis", gradeId: 1 },
+];
+
+// one to many association
+Grade.hasMany(Student);
+
+sequelize
+  .sync({ force: true })
+  .then(() => {
+    Grade.bulkCreate(grade_data, { validate: true })
+      .then(() => {
+        Student.bulkCreate(student_data, { validate: true })
+          .then(() => {
+            Grade.findAll({
+              where: {
+                grade: 9,
+              },
+              include: [
+                {
+                  model: Student,
+                },
+              ],
+            })
+              .then((result) => {
+                console.dir(result, { depth: 5 });
+              })
+              .catch((error) => {
+                console.error("Failed to retrieve data ", error);
+              });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((error) => {
+    console.error("Unable to create the table ", error);
+  });
